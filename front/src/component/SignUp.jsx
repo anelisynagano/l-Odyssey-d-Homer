6012b610851402d34b3ae7b9d7fff72bc285db04
Anelisy Nagano <anelisynagano@gmail.com>
2019-06-17 17:38:42 +0100

    create registration form

--- front/src/component/SignUp.jsx
+++ front/src/component/SignUp.jsx
@@ -4,19 +4,35 @@ class Signup extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            email: "test@test.com",
+            email: "",
+            password: "",
+            passwordcheck: "",
+            name: "",
+            lastname: ""
         };
     }
 
-    updateEmailField = (e) => {
-        this.setState({ email: e.target.value });
+    updateField = (e) => {
+        this.setState({[e.target.name]: e.target.value});
+    }
+
+    handleSubmit = (e) => {
+        console.log(JSON.stringify(this.state));
+        e.preventDefault();
     }
     
     render() {
     return(
     <div>
-        <h1>{this.state.email}</h1>
-        <input type="email" name="email" onChange={this.updateEmailField} /> 
+        <h1>{JSON.stringify(this.state)}</h1>
+        <form onSubmit={this.handleSubmit}>
+            <input placeholder="email" type="email" name="email" onChange={this.updateField} /> <br/>
+            <input placeholder="password" type="password" name="password" onChange={this.updateField} /> <br/>
+            <input placeholder="password again" type="password" name="passwordcheck" onChange={this.updateField} /> <br/>
+            <input placeholder="name" type="name" name="name" onChange={this.updateField} /> <br/>
+            <input placeholder="last name" type="lastname" name="lastname" onChange={this.updateField} /> <br/>
+            <input type="submit" value="Submit"/>
+        </form>
     </div>
     );
   }
