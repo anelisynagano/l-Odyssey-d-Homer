e4797f679bf014528397676ec45704530084524d
Anelisy Nagano <anelisynagano@gmail.com>
2019-07-08 09:19:41 +0100

    quest 050

--- back/routes/auth/auth.js
+++ back/routes/auth/auth.js
@@ -1,29 +1,29 @@
 const express = require('express');
 const router = express.Router();
-
 const connection = require('../../helpers/db');
-  
-router.post('/signup', function(req, res, next) {
 
-    var user  = [
-        req.body.email,
-        req.body.password,
-        req.body.name,
-        req.body.lastname
-    ];
-    connection.query(
-        'INSERT INTO users SET email= ?, password=?, name=?, lastname=?',
-        user,
-        function (error, results, fields) {
-            console.log(results, fields);
-            if(error) {
-                res.status(500).end();
-                console.log(error);
+
+router.post('/signup', function(req, res, next) {
+    console.log('req: ', req.body.email);
+    const user  = { 
+        email: req.body.email, 
+        password: req.body.password, 
+        name: req.body.name, 
+        lastname: req.body.lastname 
+    };
+    connection.query('INSERT INTO users SET ?', user, function(
+        error,
+        results,
+        fields,
+        ) {
+            if (error){
+                res.status(500).json({ flash:  error.message });
+            } else{
+                res.status(200).json({ flash:  "User has been signed up!" });
             }
             res.end();
         }
     );
 });
-
 
 module.exports = router;
--- front/src/component/SignUp.jsx
+++ front/src/component/SignUp.jsx
@@ -1,41 +1,65 @@
 import React, {Component} from 'react';
 
-class Signup extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            email: "",
-            password: "",
-            passwordcheck: "",
-            name: "",
-            lastname: ""
-        };
+class SignUp extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      email: 'example@example.com',
+      password: 'mypassword',
+      verificationPassword: 'mypassword',
+      name: 'jon',
+      lastname: 'doe',
+      flash: ''
     }
+  }
 
-    updateField = (e) => {
-        this.setState({[e.target.name]: e.target.value});
-    }
+  updateField = (e) => {
+    this.setState({[e.target.name]: e.target.value});
+  }
 
-    handleSubmit = (e) => {
-        console.log(JSON.stringify(this.state));
-        e.preventDefault();
-    }
-    
-    render() {
+
+  handleSubmit = (e) => {
+    fetch("/auth/signup",
+    {
+          method:  'POST',
+          headers:  new Headers({
+                'Content-Type':  'application/json'
+          }),
+          body:  JSON.stringify(this.state),
+    })
+    .then(res  =>  res.json())
+    .then(
+          res  =>  this.setState({flash:  res.flash}),
+          err  =>  this.setState({flash:  err.flash})
+    )
+    console.log(`information submitted: ${JSON.stringify(this.state)}`);
+    e.preventDefault();
+  }
+
+  render(){
     return(
-    <div>
-        <h1>{JSON.stringify(this.state)}</h1>
-        <form onSubmit={this.handleSubmit}>
-            <input placeholder="email" type="email" name="email" onChange={this.updateField} /> <br/>
-            <input placeholder="password" type="password" name="password" onChange={this.updateField} /> <br/>
-            <input placeholder="password again" type="password" name="passwordcheck" onChange={this.updateField} /> <br/>
-            <input placeholder="name" type="name" name="name" onChange={this.updateField} /> <br/>
-            <input placeholder="last name" type="lastname" name="lastname" onChange={this.updateField} /> <br/>
-            <input type="submit" value="Submit"/>
+      <div>
+        <div className={this.state.flash === "User has been signed up!" ? 'green': 'red'}>
+        <p>{this.state.flash}</p>
+        </div>
+        <h1>Sign Up</h1>
+        <form action="POST" onSubmit={this.handleSubmit}>
+          <input type="email" name="email" onChange={this.updateField} placeholder='example@example.com'/>
+          <br/>
+          <input type="password" name="password" onChange={this.updateField} placeholder='password'/>
+          <br/>
+          <input type="password" name="verificationPassword" onChange={this.updateField} placeholder='password'/>
+          <br/>
+          <input type="name" name="name" onChange={this.updateField} placeholder='John'/>
+          <br/>
+          <input type="lastname" name="lastname" onChange={this.updateField} placeholder='Doe'/>
+          <br/>
+          <input type="submit" value='Submit'/>
+
         </form>
-    </div>
+      </div>
     );
   }
 }
 
-export default Signup;
+export default SignUp;
